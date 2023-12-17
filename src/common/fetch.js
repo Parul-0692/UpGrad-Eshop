438615052607d5c2d26b504c9de932c71fe4bf71
Diaaablo <khatri.parul0692@gmail.com>
2023-12-17 23:12:05 +0530

    Modified backend application port

--- src/common/fetch.js
+++ src/common/fetch.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiService = axios.create({
-    baseURL: 'http://localhost:8081/api/',
+    baseURL: 'http://localhost:8080/api/',
 });
 const post_data = (endpoint, data, _headers) => {
     var hdrs = { 'Content-Type': 'application/json', };
